1c91eace9b88fd314b209cd4a3cb002f107ba3e2
jstringara <62561822+jstringara@users.noreply.github.com>
2024-04-05 10:31:49 +0200

    Refactor the matrix class to use the Vec class underneath

--- matrix/matrix.hpp
+++ matrix/matrix.hpp
@@ -1,25 +1,24 @@
 #ifndef MATRIX_HH
 #define MATRIX_HH
 
-#include <istream>
 #include <vector>
-#include <sstream>
 #include <iostream>
+
+#include "vec.hpp"
 
 using std::vector;
 
 template <class T> class Matrix {
 
     // matrix is a vector of vectors representing columns
-    typedef vector<vector<T>> container_type;
+    typedef vector<Vec<T>> container_type;
 
 public:
     typedef typename container_type::value_type column_type;
     typedef typename column_type::size_type size_type;
     typedef typename container_type::pointer pointer;
     typedef typename container_type::const_pointer const_pointer;
-    typedef typename column_type::reference reference;
-    typedef typename column_type::const_reference const_reference;
+    typedef typename column_type::value_type value_type;
 
 private:
     size_type m_rows = 0, m_columns = 0;
@@ -28,42 +27,31 @@ private:
 public:
     // constructors
     Matrix<T> (void) = default;
-    Matrix<T> (size_type rows, size_type cols, const_reference value = T());
+    Matrix<T> (size_type rows, size_type cols, value_type value = T());
     explicit Matrix<T>(size_type rows, size_type cols, container_type values);
-    explicit Matrix<T> (std::istream &);
-
-    void read (std::istream &);
 
     // elements access
-    reference operator () (size_type, size_type);
-    const_reference operator () (size_type, size_type) const;
+    column_type operator [] (size_type);
 
     // size access
     size_type rows (void) const;
     size_type columns (void) const;
 
     // row and column access
-    column_type col(size_type j) const;
-    column_type row(size_type i) const;
+    column_type col (size_type j) const;
+    column_type row (size_type i) const;
 
-    // row and column insertion
-    void insert_col(size_type j, column_type const & col);
-    void insert_row(size_type i, column_type const & row);
-
-    Matrix<T> transposed (void) const;
+    // row insertion
+    void insert_row (size_type i, column_type const & row);
 
     pointer data (void);
     const_pointer data (void) const;
 
-    void print (std::ostream&) const;
-
-    void to_csv (std::ostream&) const;
-
 };
 
 template <class T>
-Matrix<T>::Matrix (size_type rows, size_type cols, const_reference value)
-    : m_rows (rows), m_columns (cols), m_data (cols, vector<T>(rows,value)) {}
+Matrix<T>::Matrix (size_type rows, size_type cols, value_type value)
+    : m_rows (rows), m_columns (cols), m_data (cols, Vec<T>(rows, value)) {}
  
 template <class T>
 Matrix<T>::Matrix(size_type rows, size_type cols, container_type values)
@@ -73,43 +61,10 @@ Matrix<T>::Matrix(size_type rows, size_type cols, container_type values)
         throw std::invalid_argument ("Matrix::Matrix: wrong size");
 }
 
-template <class T>
-Matrix<T>::Matrix (std::istream & in) {
-    read (in);
-}
-
-template <class T>
-void Matrix<T>::read (std::istream & in) {
-    std::string line;
-    std::getline (in, line);
-
-    std::istringstream first_line(line);
-    first_line >> m_rows >> m_columns;
-    // resize the data vector
-    m_data.resize(m_columns, vector<T>(m_rows));
-
-    for (size_type i = 0; i < m_rows; ++i) {
-        std::getline (in, line);
-        std::istringstream current_line(line);
-
-        for (size_type j = 0; j < m_columns; ++j) {
-            /* alternative syntax: current_line >> operator () (i, j);
-             * or: current_line >> m_data[sub2ind (i, j)];
-             */
-            current_line >> (*this)(i, j);
-        }
-    }
-}
-
 // elements access
 template <class T>
-typename Matrix<T>::reference Matrix<T>::operator () (size_type i, size_type j) {
-    return m_data[j][i];
-}
-
-template <class T>
-typename Matrix<T>::const_reference Matrix<T>::operator () (size_type i, size_type j) const {
-    return m_data[j][i];
+typename Matrix<T>::column_type Matrix<T>::operator [] (size_type j) {
+    return m_data[j];
 }
 
 template <class T>
@@ -123,29 +78,10 @@ typename Matrix<T>::size_type Matrix<T>::columns (void) const {
 }
 
 template <class T>
-typename Matrix<T>::column_type Matrix<T>::col(size_type j) const {
-    // check that the column index is valid
-    if (j >= m_columns)
-        throw std::out_of_range ("column index out of range");
-    return m_data[j];
-}
-
-template <class T>
-void Matrix<T>::insert_col(size_type j, column_type const & col) {
-    // check that the column index is valid
-    if (j >= m_columns)
-        throw std::out_of_range ("column index out of range");
-    // check that the column size is correct
-    if (col.size() != m_rows)
-        throw std::invalid_argument ("column size mismatch");
-    m_data[j] = col;
-}
-
-template <class T>
 typename Matrix<T>::column_type Matrix<T>::row(size_type i) const {
     column_type row(m_columns); // recast
     for (size_type j = 0; j < m_columns; ++j)
-        row[j] = operator () (i, j);
+        row[j] = operator [] (i);
     return row;
 }
 
@@ -158,20 +94,7 @@ void Matrix<T>::insert_row(size_type i, column_type const & row) {
     if (row.size() != m_columns)
         throw std::invalid_argument ("row size mismatch");
     for (size_type j = 0; j < m_columns; ++j)
-        operator () (i, j) = row[j];
-}
-
-template <class T>
-Matrix<T> Matrix<T>::transposed (void) const {
-
-    // create the transposed matrix
-    Matrix At (m_columns, m_rows);
-
-    for (size_type i = 0; i < m_rows; ++i)
-        for (size_type j = 0; j < m_columns; ++j)
-            At(j, i) = operator () (i, j);
-
-    return At;
+        *this[j][i] = row[j];
 }
 
 template <class T>
@@ -185,26 +108,13 @@ typename Matrix<T>::const_pointer Matrix<T>::data (void) const {
 }
 
 template <class T>
-void Matrix<T>::print (std::ostream& os) const {
-    using size_type = typename Matrix<T>::size_type;
-
-    os << "(" << m_rows << "x" << m_columns << ")\n";
-
-    for (size_type i = 0; i < m_rows; ++i) {
-        for (size_type j = 0; j < m_columns; ++j)
-            os << operator () (i,j) << " ";
-        os << "\n";
+std::ostream& operator << (std::ostream & os, Matrix<T> const & m) {
+    for (size_t i = 0; i < m.rows(); ++i) {
+        for (size_t j = 0; j < m.columns(); ++j)
+            os << m[i][j] << " ";
+        os << std::endl;
     }
-}
-
-template <class T>
-void Matrix<T>::to_csv (std::ostream& os) const {
-    for (size_type i = 0; i < m_rows; ++i) {
-        for (size_type j = 0; j < m_columns; ++j) {
-            char del = (j < m_columns - 1) ? ',' : '\n';
-            os << operator () (i,j) << del;
-        }
-    }
+    return os;
 }
 
 template <class T>
@@ -221,7 +131,7 @@ Matrix<T> operator * (Matrix<T> const & A, Matrix<T> const & B) {
     for (size_type i = 0; i < A.rows (); ++i)
         for (size_type j = 0; j < B.columns (); ++j)
             for (size_type k = 0; k < A.columns (); ++k)
-                C(i, j) += A(i, k) * B(k, j);
+                C[i][j] += A[i][k] * B[k][j];
 
     return C;
 }
